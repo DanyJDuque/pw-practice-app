e79243dc09b5eebdba4871b694264e15104b5c06
Dany Duque <danyjduqued@gmail.com>
2026-03-12 15:16:50 -0300

    Section 6 lesson 51 Page Objects Manager

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,30 +1,29 @@
-import { test, expect } from '@playwright/test'
-import { NavigationPage } from '../page-objects/navigationPage'
-import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
-import {DatapickerPage} from '../page-objects/datepickerPage'
+import { test } from '@playwright/test'
+import { PageManager } from '../page-objects/pageManager'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
 })
 
 test('navigate to form page', async ({ page }) => {
-    const navigateTo = new NavigationPage(page)
+    // const navigateTo = new NavigationPage(page)
+    const pm = new PageManager(page)
 
-    await navigateTo.formLayoutPage()
-    await navigateTo.datePickerPage()
-    await navigateTo.smartTablePage()
-    await navigateTo.toastrPage()
-    await navigateTo.tooltipPage()
+    await pm.navigateTo().formLayoutPage()
+    await pm.navigateTo().datePickerPage()
+    await pm.navigateTo().smartTablePage()
+    await pm.navigateTo().toastrPage()
+    await pm.navigateTo().tooltipPage()
+    
 })
 
 test('parametrized methods', async ({ page }) => {
-    const navigateTo = new NavigationPage(page)
-    const onFormLayoutsPage = new FormLayoutsPage(page)
-    const onDatepickerPage = new DatapickerPage(page)    
-    await navigateTo.formLayoutPage()
-    await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectionOption('test@test.com', 'Welcome1', 'Option 1')
-    await onFormLayoutsPage.submitInlineFormNameEmailAndCheckbox('Jane Doe', 'janeDoe@test.com', false)
-    await navigateTo.datePickerPage()
-    await onDatepickerPage.selectCommonDatePickerDateFromToday(200)
-    await onDatepickerPage.selectDatepickerWithRangeFromToday(5,10)
+    const pm = new PageManager(page)
+
+    await pm.navigateTo().formLayoutPage()
+    await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectionOption('test@test.com', 'Welcome1', 'Option 1')
+    await pm.onFormLayoutsPage().submitInlineFormNameEmailAndCheckbox('Jane Doe', 'janeDoe@test.com', false)
+    await pm.navigateTo().datePickerPage()
+    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(200)
+    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(5, 10)
 })
