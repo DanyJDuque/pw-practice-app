97874005e4625a2bef6d597cfd5a93be23fc1ce5
Dany Duque <danyjduqued@gmail.com>
2026-03-15 14:56:07 -0300

    update navigationPage

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -8,32 +8,32 @@ export class NavigationPage extends HelperBase {
     }
 
     async formLayoutPage() {
-        await this.selectGroupManuItem('Forms')
+        await this.selectGroupMenuItem('Forms')
         await this.page.getByText('Form Layouts').click()
         await this.waitForNumberOfSeconds(2)
     }
 
     async datePickerPage() {
-        await this.selectGroupManuItem('Forms')
+        await this.selectGroupMenuItem('Forms')
         await this.page.getByText('Datepicker').click()
     }
 
     async smartTablePage() {
-        await this.selectGroupManuItem('Tables & Data')
+        await this.selectGroupMenuItem('Tables & Data')
         await this.page.getByText('Smart Table').click()
     }
 
     async toastrPage() {
-        await this.selectGroupManuItem('Modal & Overlays')
+        await this.selectGroupMenuItem('Modal & Overlays')
         await this.page.getByText('Toastr').click()
     }
 
     async tooltipPage() {
-        await this.selectGroupManuItem('Modal & Overlays')
+        await this.selectGroupMenuItem('Modal & Overlays')
         await this.page.getByText('Tooltip').click()
     }
 
-    private async selectGroupManuItem(groupItemTitle: string) {
+    private async selectGroupMenuItem(groupItemTitle: string) {
         const groupMenuItem = this.page.getByTitle(groupItemTitle)
         const expandedState = await groupMenuItem.getAttribute('aria-expanded')
         if (expandedState == "false") {
