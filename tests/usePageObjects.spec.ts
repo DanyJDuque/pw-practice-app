a94297dbd53dab8e5a0bfdbc7e6a99039e8a3a55
DanyJDuque <djduque@gmail.com>
2026-04-14 14:55:04 -0400

    62. Test Data Generator

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,7 @@
 import { test } from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
+import {faker} from '@faker-js/faker'
+
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -20,10 +22,16 @@ test('navigate to form page', async ({ page }) => {
 test('parametrized methods', async ({ page }) => {
     const pm = new PageManager(page)
 
+    const randomFullName = faker.person.fullName()
+    // const randomEmail = `${randomFullName}${faker.number.int(1000)}@test.com`.toLowerCase().replace(/\s/g, '')
+    const randomEmail = `${randomFullName.replace(' ','')}${faker.number.int(1000)}@test.com`
+
+
     await pm.navigateTo().formLayoutPage()
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectionOption('test@test.com', 'Welcome1', 'Option 1')
-    await pm.onFormLayoutsPage().submitInlineFormNameEmailAndCheckbox('Jane Doe', 'janeDoe@test.com', false)
-    await pm.navigateTo().datePickerPage()
-    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(200)
-    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(5, 10)
+    // await pm.onFormLayoutsPage().submitInlineFormNameEmailAndCheckbox('Jane Doe', 'janeDoe@test.com', false)
+    await pm.onFormLayoutsPage().submitInlineFormNameEmailAndCheckbox(randomFullName, randomEmail, false)
+    // await pm.navigateTo().datePickerPage()
+    // await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(200)
+    // await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(5, 10)
 })
