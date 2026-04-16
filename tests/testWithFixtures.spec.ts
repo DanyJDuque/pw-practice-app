cfdd197686721c7473ba8e41e7ebb7dcc52645da
Dany Duque <danyjduqued@gmail.com>
2026-04-16 16:43:58 -0400

    68. Fixtures

---
+++ tests/testWithFixtures.spec.ts
@@ -0,0 +1,12 @@
+// import { test } from '@playwright/test'
+import { test } from '../test-options'
+// import { PageManager } from '../page-objects/pageManager'
+import { faker } from '@faker-js/faker'
+
+test('parametrized methods', async ({ pageManager }) => {
+    const randomFullName = faker.person.fullName()
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`
+
+    await pageManager.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectionOption(process.env.USERNAME, process.env.PASSWORD, 'Option 1')
+    await pageManager.onFormLayoutsPage().submitInlineFormNameEmailAndCheckbox(randomFullName, randomEmail, false)
+})
