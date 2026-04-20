4c61f4420c535697932f15b618a8ca9eb29f444e
Dany Duque <danyjduqued@gmail.com>
2026-04-19 20:09:02 -0400

    added argos token

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,10 @@ export default defineConfig<TestOptions>({
       ({
         // Upload to Argos on CI only.
         uploadToArgos: !!process.env.CI,
+
+          // Set your Argos token (required if not using GitHub Actions).
+        // token: "<YOUR-ARGOS-TOKEN>",
+        token: process.env.ARGOS_TOKEN,
       }),
     ],
     ['json', { outputFile: 'test-results/jsonReport.json' }],
